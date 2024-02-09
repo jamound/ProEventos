cd58afafa5a86ebb4eaa4fb25189159b0458719e
Ramon Mançano <rmancano@trustprev.com.br>
2024-02-09 19:07:11 -0300

    commit inicial

---
+++ Back/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _eventos { get; set; } = new Evento[]
+        {
+            new Evento
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2),
+                ImagemUrl = "foto1.png"
+            },
+            new Evento
+            {
+                EventoId = 2,
+                Tema = "Angular e suas novidades",
+                Local = "São Paulo",
+                Lote = "2º Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(3),
+                ImagemUrl = "foto2.png"
+            }
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _eventos.Where(evento => evento.EventoId == id).FirstOrDefault();
+        }
+    }
+}
---
+++ Back/ProEventos.API/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProEventos.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
